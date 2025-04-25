250509ed078b2965569f1b90fa70f6e6bac608cf
Mo7amed El4nawy <101730265+Mohammedel4nawy@users.noreply.github.com>
2025-04-25 17:07:02 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,46 +1,95 @@
-// start navbar ----------------------------------------------------------------------------------------
-
-window.addEventListener("scroll", () => {
-  let scrolly = window.scrollY;
-  if (scrolly > 500) {
-    document.querySelector(".navbar").classList.remove("bg-light");
-
+// start nav bar animation ------------------------------------------------
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 500) {
     document.querySelector(".navbar").classList.add("bg-danger");
+    document.querySelector(".navbar").classList.remove("bg-light");
   } else {
     document.querySelector(".navbar").classList.remove("bg-danger");
     document.querySelector(".navbar").classList.add("bg-light");
   }
-  console.log(scrolly);
 });
 
-// end nav bar ----------------------------------------------------------------------------------------
+// handling bullits -------------------------------------------------------------------------
 
-// start timer ----------------------------------------------------------------------------------------
+let bullet = document.querySelectorAll(".bullet");
 
-let a = setInterval(null, 1000);
-function Datee() {
-  let da = new Date();
-  document.querySelector(".date").innerHTML = da.toLocaleTimeString("en-US", {
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: true,
+bullet.forEach((a) => {
+  a.addEventListener("click", function () {
+    bullet.forEach((b) => {
+      b.classList.remove("active");
+    });
+    this.classList.add("active");
+  });
+});
+//============================================================================================
+
+// handling date and time ------------------------------------------------------------------
+let setI = setInterval(null, 1000);
+
+function showData() {
+  let DateNow = new Date();
+  bullet.forEach((e) => {
+    if (e.classList.contains("active")) {
+      if (e.getAttribute("data-type") == "sec") {
+        document.querySelector(".date").innerHTML = DateNow.toLocaleTimeString(
+          "en-US",
+          {
+            second: "2-digit",
+            hour12: true,
+          }
+        );
+        document.querySelector(".date").classList.add("bg-black");
+      } else if (e.getAttribute("data-type") == "min") {
+        document.querySelector(".date").innerHTML = DateNow.toLocaleTimeString(
+          "en-US",
+          {
+            minute: "2-digit",
+            hour12: true,
+          }
+        );
+        document.querySelector(".date").classList.add("bg-black");
+      } else if (e.getAttribute("data-type") == "hour") {
+        document.querySelector(".date").innerHTML = DateNow.toLocaleTimeString(
+          "en-US",
+          {
+            hour: "2-digit",
+            hour12: true,
+          }
+        );
+        document.querySelector(".date").classList.add("bg-black");
+      } else if (e.getAttribute("data-type") == "all") {
+        document.querySelector(".date").innerHTML = DateNow.toLocaleTimeString(
+          "en-US",
+          {
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            hour12: true,
+          }
+        );
+        document.querySelector(".date").classList.add("bg-black");
+      }
+    }
   });
 }
 
-x = true;
+//============================================================================================
 
-function getDate() {
-  if (x == true) {
-    a = setInterval(Datee, 1000);
-    x = false;
-    document.querySelector(".date").classList.add("bg-black");
-  } else if (x == false) {
-    clearInterval(a);
-    x = true;
+// handling start and stop button ---------------------------------------------------------
+
+let swicher = true;
+
+function start() {
+  if (swicher) {
+    setI = setInterval(showData, 1000);
+    swicher = false;
+  } else if (swicher == false) {
+    clearInterval(setI);
+    swicher = true;
   }
 }
 
-document.querySelector(".button").addEventListener("click", getDate);
-
-// end timer ----------------------------------------------------------------------------------------
+document.querySelector(".button").addEventListener("click", function () {
+  start();
+});
+//============================================================================================
